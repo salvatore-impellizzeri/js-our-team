ba2ea1f574551b5a8713c1d5134b835ad49d57a6
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-07-25 16:58:39 +0200

    Milestone 0 e 1

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+const team = [
+    {
+        firstName: 'Wayne',
+        lastName: 'Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        firstName: 'Angela',
+        lastName: 'Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        firstName: 'Walter',
+        lastName: 'Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        firstName: 'Angela',
+        lastName: 'Lopez',
+        role: 'Social Media Manager',
+        image: 'wangela-lopez-social-media-manager.jpg',
+    },
+    {
+        firstName: 'Scott',
+        lastName: 'Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    {
+        firstName: 'Barbara',
+        lastName: 'Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+console.log(team);
