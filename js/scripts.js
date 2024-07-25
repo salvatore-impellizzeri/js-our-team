d0359537cad5b29a5c53d870d31f955995043111
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-07-25 17:35:46 +0200

    Inserite immagini

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,61 @@
+// OGGETTO TEAM
+
 const team = [
     {
         firstName: 'Wayne',
         lastName: 'Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpg',
+        image: 'img/wayne-barnett-founder-ceo.jpg',
     },
     {
         firstName: 'Angela',
         lastName: 'Caroll',
         role: 'Chief Editor',
-        image: 'angela-caroll-chief-editor.jpg',
+        image: 'img/angela-caroll-chief-editor.jpg',
     },
     {
         firstName: 'Walter',
         lastName: 'Gordon',
         role: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpg',
+        image: 'img/walter-gordon-office-manager.jpg',
     },
     {
         firstName: 'Angela',
         lastName: 'Lopez',
         role: 'Social Media Manager',
-        image: 'wangela-lopez-social-media-manager.jpg',
+        image: 'img/angela-lopez-social-media-manager.jpg',
     },
     {
         firstName: 'Scott',
         lastName: 'Estrada',
         role: 'Developer',
-        image: 'scott-estrada-developer.jpg',
+        image: 'img/scott-estrada-developer.jpg',
     },
     {
         firstName: 'Barbara',
         lastName: 'Ramos',
         role: 'Graphic Designer',
-        image: 'barbara-ramos-graphic-designer.jpg',
+        image: 'img/barbara-ramos-graphic-designer.jpg',
     },
 ];
 
 console.log(team);
+const mainCont = document.querySelector(".main-container");
+const cardArray = []
+
+// CREAZIONE CARD
+
+for(let i = 0; i < team.length; i++){
+    let card = document.createElement("div");
+    card.classList.add("my-card");
+    mainCont.append(card);
+    cardArray.push(card);
+
+    // INSERIMENTO IMMAGINI
+
+    let img = document.createElement("img");
+    img.classList.add("w-100");
+    img.src = team[i].image;
+    
+    card.append(img)
+}
